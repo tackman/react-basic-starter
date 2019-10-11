cc678fbcc96ed2d21a858dc7525810728de602db
tackman <tackman@tackman.info>
2019-10-11 22:06:25 +0900

    update node_modules versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
@@ -65,7 +65,7 @@ module.exports = (_env, options) => ({
     }
   },
   plugins: [
-    new CleanWebpackPlugin(["build"]),
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: "styles.css"
     }),
